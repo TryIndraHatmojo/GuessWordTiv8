a6a02de03170fbcb81b137bdd3039214a47d28d3
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-05-04 22:59:11 +0700

    CRU List Question - indra

--- js/list-questions-menu.js
+++ js/list-questions-menu.js
@@ -1,6 +1,24 @@
-function getListQuestions(){
+const createModal = new bootstrap.Modal('#createModal', {
+    keyboard: false
+})
+
+const editModal = new bootstrap.Modal('#editModal', {
+    keyboard: false
+})
+
+function getLocalStorageQuestions(){
     let questions = localStorage.getItem("data-questions")
     questions = JSON.parse(questions)
+    return questions
+}
+
+function saveLocalStorageQuestions(question){
+    console.log(question)
+    localStorage.setItem("data-questions", JSON.stringify(question))
+}
+
+function getListQuestions(){
+    let questions = getLocalStorageQuestions()
     let newQuestions = [...questions].sort((a,b)=>b.id-a.id)
     let row = ""
     let nomor = 1
@@ -14,8 +32,11 @@ function getListQuestions(){
             <td>${question.answer}</td>
             <td>${question.hint}</td>
             <td>
-                <a href="javascript:;"class="button red reset-answer" onclick="hapusQuestion(${question.id})">
-                    Hapus
+                <a href="javascript:;"class="button blue btn-edit" onclick="editQuestion(${question.id})">
+                    <box-icon name='pencil' color='#fffbfb' ></box-icon>
+                </a>
+                <a href="javascript:;"class="button red btn-trash" onclick="hapusQuestion(${question.id})">
+                    <box-icon name='trash' type='solid' color='#fffbfb' ></box-icon>
                 </a>
             </td>
         </tr>
@@ -23,6 +44,72 @@ function getListQuestions(){
         nomor++
     }
     document.getElementById("table-body-list-questions").innerHTML = row
+}
+
+function createQuestion(){
+    document.getElementById("question-edit").value = ""
+    document.getElementById("category-edit").value = ""
+    document.getElementById("answer-edit").value = ""
+    document.getElementById("hint-edit").value = ""
+
+    createModal.show()
+}
+
+function storeQuestion(){
+    let question = document.getElementById("question-create").value
+    let category = document.getElementById("category-create").value
+    let answer = document.getElementById("answer-create").value
+    let hint = document.getElementById("hint-create").value
+
+    let questions = getLocalStorageQuestions()
+    let item = {
+        id: questions.length+1,
+        question: question,
+        category: category,
+        answer: answer,
+        hint: hint
+    }
+    questions.push(item)
+
+    saveLocalStorageQuestions(questions)
+    
+    getListQuestions()
+}
+
+function editQuestion(id){
+    let questions = getLocalStorageQuestions()
+    let item = questions.find(questions=>questions.id === id)
+    
+    document.getElementById("id-edit").value = item.id
+    document.getElementById("question-edit").value = item.question
+    document.getElementById("category-edit").value = item.category
+    document.getElementById("answer-edit").value = item.answer
+    document.getElementById("hint-edit").value = item.hint
+
+    editModal.show()
+}
+
+function updateQuestion(){
+    let id = document.getElementById("id-edit").value
+    let question = document.getElementById("question-edit").value
+    let category = document.getElementById("category-edit").value
+    let answer = document.getElementById("answer-edit").value
+    let hint = document.getElementById("hint-edit").value
+
+    let questions = getLocalStorageQuestions()
+    let item = questions.find(questions=>questions.id === Number(id))
+    item.question = question
+    item.category = category
+    item.answer = answer
+    item.hint = hint
+
+    console.log(questions)
+
+    saveLocalStorageQuestions(questions)
+    
+    getListQuestions()
+
+    editModal.hide()
 }
 
 function hapusQuestion(id){
@@ -33,28 +120,7 @@ function hapusQuestion(id){
 
 
     // buat update HTML yang baru
-    let newQuestions = [...questions].sort((a,b)=>b.id-a.id)
-    let row = ""
-    let nomor = 1
-    for(let i = 0;i < newQuestions.length;i++){
-        let question = newQuestions[i]
-        row += `
-        <tr>
-            <td>${nomor}</td>
-            <td>${question.question}</td>
-            <td>${question.category}</td>
-            <td>${question.answer}</td>
-            <td>${question.hint}</td>
-            <td>
-                <a href="javascript:;"class="button red reset-answer" onclick="hapusQuestion(${question.id})">
-                    Hapus
-                </a>
-            </td>
-        </tr>
-        `
-        nomor++
-    }
-    document.getElementById("table-body-list-questions").innerHTML = row
+    getListQuestions()
 }
 
 window.onload = function(){
