2f5ff11e96d8bbe2436dcc273312fb6c1d8d4ddb
Elang Noor Aluda <elangnoor01@gmail.com>
2025-05-05 18:20:03 +0700

    update audio dan alert game-result page

--- js/game-result.js
+++ js/game-result.js
@@ -9,22 +9,25 @@ function saveScore() {
   let htmlNama = document.getElementById("nama").value;
   
   if (htmlNama === "") {
-    alert("Nama harus di isi");
-  } else if (htmlNama !== "") {
+    alert("Please fill in your name first!")
+  }
+  
+  if (htmlNama !== "") {
+    alert(`Thank You For Playing Our Game ${htmlNama}`)
+    
     let dataHighScore = localStorage.getItem("data-high-score");
     dataHighScore = JSON.parse(dataHighScore);
 
     let newUserScore = { name: htmlNama, score: score };
     dataHighScore.push(newUserScore);
     localStorage.setItem("data-high-score", JSON.stringify(dataHighScore));
-    window.location.href = ("high-score.html");
+    window.location.href = "high-score.html";
   }
 }
 
 function backToMenu() {
-    window.location.href = ("index.html")  
+  window.location.href = "index.html";
 }
-
 
 window.onload = function () {
   setScore();
